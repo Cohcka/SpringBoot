50f79b9eac83105a90645bb8b945ccea10ea8935
MaxCohcka <Cohcka@gmail.com>
2020-12-25 22:47:11 +0300

    startHWSpringData

---
+++ src/test/java/ru/maxcorp/springdata/SpringDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maxcorp.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
